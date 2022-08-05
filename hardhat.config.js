5df16692501a895a7f7753086677dccb15e02c2f
Haoyang Liu <haoyang.liu@fdmgroup.com>
2022-08-04 20:41:44 -0400

    Adding coverage, gas reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,9 +2,12 @@ require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 require("@nomiclabs/hardhat-etherscan")
 require("./tasks/block-number")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     defaultNetwork: "hardhat",
@@ -14,9 +17,20 @@ module.exports = {
             accounts: [PRIVATE_KEY],
             chainId: 4,
         },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+        },
     },
     solidity: "0.8.8",
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
     },
 }
